a3ec3c343a440fb95b48ac5338b462689b2a3544
Elise Fung <elisefung8@gmail.com>
2015-06-15 23:42:20 -0700

    added about page

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$('.menu-icon').click(function() {
+    $('.menu').toggle();
+});
